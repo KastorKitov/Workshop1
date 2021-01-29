41600743bbe9e2fe97a71bf401d235b221f93491
Kastor Kitov <kitov.kastor@gmail.com>
2021-01-29 18:10:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const config = require('./config/config');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.send('its working')
+});
+
 app.listen(config.PORT,() => console.log(`Server is running on port: ${config.PORT}...`))
